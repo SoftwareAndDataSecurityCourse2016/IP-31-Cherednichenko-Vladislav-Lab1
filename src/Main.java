ca622c9d76363fafb6cdf91c20b05f4b36b9ea3e
Vladislav Cherednichenko <chervjak5@gmail.com>
2016-12-29 18:03:23 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+				
+		String task2 = FileWorker.loadTask("task/task2.txt");
+		System.out.println(task2);
+		Decryptor decryptor = new ViginerDecryptor(task2);
+		System.out.println(decryptor.decrypt());
+		decryptor.printKey();
+	}	
+	
+	
+	public static int gcd(int a, int b) {
+	    if (b == 0) return a;
+	    int x = a % b;
+	    return gcd(b, x);
+	}		
+}
